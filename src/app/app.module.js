cc6b7def9939cdcf286639f837ee5c9a7f23417a
ttaylor272 <ttaylor272@gmail.com>
2016-05-05 17:25:02 -0400

    First commit

---
+++ src/app/app.module.js
@@ -0,0 +1,17 @@
+(function() {
+    'use strict';
+
+angular
+.module('app', [
+    //Angular modules
+    'ngRoute',
+    
+    // Third-party modules
+    'firebase',
+    
+    //Custom modules
+    'app.landing' ,
+    'app.waitList'
+    ]);
+    
+})();
